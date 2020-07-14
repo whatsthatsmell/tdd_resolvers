faaf8e29c695a616668e94181017c6ba9eaef4e2
Joel Palmer <joelpalmer@gmail.com>
2020-07-14 15:50:42 -0500

    setup linting...

---
+++ schema/index.js
@@ -0,0 +1,9 @@
+const dataSource = require('./dataSource')
+const resolvers = require('./resolvers')
+const typeDefs = require('./typeDefs')
+
+module.exports = {
+  dataSource,
+  resolvers,
+  typeDefs
+}
