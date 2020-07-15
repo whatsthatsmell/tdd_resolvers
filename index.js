1b63dbfa0605f37dc3bd79c5abfa61b837bd534f
Joel Palmer <joelpalmer@users.noreply.github.com>
2020-07-15 07:57:06 -0500

    feat: add schema

---
+++ index.js
@@ -0,0 +1,9 @@
+// Apollo is overkill, we could just use graphql-js and graphql-tag
+const { ApolloServer } = require('apollo-server')
+const { typeDefs, resolvers } = require('./schema')
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => {
+  console.log('A running server at: ' + url)
+})
+
+// curl -s 'http://localhost:4000/' -H 'Content-Type: application/json' -d '{"query": "{ shows {name, genre}}"}' | jq
---
+++ schema/dataSource.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    name: 'Superstore',
+    genre: 'comedy'
+  },
+  {
+    name: 'Mom',
+    genre: 'comedy'
+  }
+]
